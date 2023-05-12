4be4ee257aa5e912c18fb67975aece3b67800417
HYEEYH <hye890@daum.net>
2023-05-12 17:54:33 +0900

    20230512

---
+++ Data_learning_study/try.py
@@ -0,0 +1,23 @@
+# # 코드 따라쓰기 복습
+
+# # import seaborn as sns
+# # df_titanic = sns.load_dataset
+# # sns. catplot( x = 'class', col = 'who', kind = 'count', data = df_titanic)
+
+
+# # import numpy as np
+# # import matplotlib.pyplot as plt
+
+# import sklearn
+# # print(sklearn.__version__)
+# import pandas as pd
+# import seaborn as sns
+# from sklearn import datasets
+# from sklearn.linear_model import LinearRegression
+
+
+# boston = datasets.load_boston()
+# # print(type(boston))
+# # print('\n')
+# # print(boston.data.shape)
+# print(boston.feature_names)
