02f4a838c5649b506468f377ed3388a2f6e06d20
HYEEYH <hye890@daum.net>
2023-05-17 17:44:51 +0900

    연습장

---
+++ python_study/z_codingtest_practice.py
@@ -0,0 +1,36 @@
+# def solution(angle):
+#     0 < angle <= 180
+#     if 0 < angle < 90:
+#         return 1
+#     elif angle == 90:
+#         return 2
+#     elif 90 < angle < 180:
+#         return 3
+#     else: 
+#         return 4
+
+
+
+
+" n 이하의 짝수를 모두 더하기"
+# sum = 0
+# for i in range(0,10,2):
+#     sum = sum+i
+# print(sum)
+
+"정수 배열 numbers가 매개변수로 주어집니다. "
+"numbers의 원소의 평균값을 return"
+
+
+
+
+# .lower()
+
+
+
+# my_list = [10, 20, 30, 40, 50, 60]
+# mean = sum(my_list) / len(my_list)
+
+
+
+
